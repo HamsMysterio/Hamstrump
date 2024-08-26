4a0e36c0429c5dc1cdab6876557ae5c06b5f6bed
HamsMysterio <hamstrump@gmail.com>
2024-08-26 13:49:25 +0200

    Add files via upload

--- _app/env.js
+++ _app/env.js
@@ -1,1 +1,1 @@
-export const env={}
+export const env={"PUBLIC_WALLETCONNECT_ID":"75ce17bae1bef3e597b12a2c8f6f570f","PUBLIC_ALCHEMY_ID":"qqelnmu5cwczpa51"}
---
+++ _app/immutable/entry/start.BQ1QHdgx.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.vbZQ2-iI.js";export{t as start};
